b3aefb4df21f6d30080d6b5ac9eb3219d509c88c
ティナ <87232401+fr0m-h3ll@users.noreply.github.com>
2025-03-28 16:56:32 +0330

    creating OrderController

---
+++ E-commerce/API/Controllers/OrderController.cs
@@ -0,0 +1,113 @@
+﻿using E_commerce.Application.Contracts;
+using E_commerce.Application.DTOs.OrderHeaderDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        #region [- Ctor() -]
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        #endregion
+
+        #region [- Get() -]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(GetOrderHeaderServiceDto dto)
+        {
+            Guard_OrderService();
+            var getResponse = await _orderService.Get(dto);
+            var response = getResponse.Value;
+            if (response is null)
+            {
+                return Json("NotFound");
+            }
+            return Json(response);
+        }
+        #endregion
+
+        #region [- GetAll() -]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            Guard_OrderService();
+            var getAllResponse = await _orderService.GetAll();
+            var response = getAllResponse.Value.GetOrderHeaderServiceDtos;
+            return Json(response);
+        }
+        #endregion
+
+        #region [- Post() -]
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] PostOrderHeaderServiceDto dto)
+        {
+            Guard_OrderService();
+            var postDto = new GetOrderHeaderServiceDto() { Id = dto.Id };
+            var getResponse = await _orderService.Get(postDto);
+
+            switch (ModelState.IsValid)
+            {
+                case true when getResponse.Value is null:
+                    {
+
+                        var postResponse = await _orderService.Post(dto);
+                        return postResponse.IsSuccessful ? Ok() : BadRequest();
+                    }
+                case true when getResponse.Value is not null:
+                    return Conflict(dto);
+                default:
+                    return BadRequest();
+            }
+        }
+        #endregion
+
+        #region [- Put() -]
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] PutOrderHeaderServiceDto dto)
+        {
+            Guard_OrderService();
+
+            if (ModelState.IsValid)
+            {
+                var existingProduct = await _orderService.Get(new GetOrderHeaderServiceDto { Id = dto.Id });
+                if (existingProduct.Value != null && existingProduct.Value.Id != dto.Id)
+                {
+                    return Conflict("Email already exists.");
+                }
+                var putResponse = await _orderService.Put(dto);
+                return putResponse.IsSuccessful ? Ok() : BadRequest();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+
+        #region [- Delete() -]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromBody] DeleteOrderHeaderServiceDto dto)
+        {
+            Guard_OrderService();
+            var deleteResponse = await _orderService.Delete(dto);
+            return deleteResponse.IsSuccessful ? Ok() : BadRequest();
+        }
+        #endregion
+
+        #region [- ProductServiceGuard() -]
+        private ObjectResult Guard_OrderService()
+        {
+            if (_orderService is null)
+            {
+                return Problem($" {nameof(_orderService)} is null.");
+            }
+
+            return null;
+        }
+        #endregion
+    }
+}
--- E-commerce/API/obj/Debug/net9.0/E-commerce.Api.AssemblyInfo.cs
+++ E-commerce/API/obj/Debug/net9.0/E-commerce.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77978bcba04db4f4bafac0c0f6a3290929965e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64916e666913bfbad9dcc5e43793bc29688e1322")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
